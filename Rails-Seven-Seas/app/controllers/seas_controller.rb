c5824a8edaae738c64653f34cf436379e5c58358
Srishti Nath <srishti.nath@gmail.com>
2020-05-14 16:03:42 -0400

    first pairing commit

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,42 @@
 class SeasController < ApplicationController
   #define your controller actions here
 
+  def welcome
+  end
+
+  def index
+    @seas = Sea.all
+    render :index
+  end
+  
+  def new
+    @sea = Sea.new
+  end
+  
+  def create
+    @sea = Sea.create(sea_params)
+    redirect_to seas_path(@sea)
+  end
+  
+  def edit
+    @sea = Sea.find(sea_params[:id])
+  end
+  
+  def update
+    @sea = Sea.find(sea_params[:id])
+    @sea.update(sea_params)
+    redirect_to seas_path(@sea)
+  end
+  
+  def show
+    @sea = Sea.find(sea_params[:id])
+  end
+  
+  def destroy
+    @sea = Sea.find(sea_params[:id])
+    @sea.destroy
+    redirect_to seas_path
+  end
 
   private
   # In controller actions, use this private method to access sea params from forms.
@@ -9,5 +45,4 @@ class SeasController < ApplicationController
   def sea_params
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
-
 end
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  resources :seas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/', to: "seas#welcome"
+
+  # #CREATE
+  # get 'seas/new', to: "seas#new"
+  # # , as: 'new_sea_path'
+  # post 'seas', to: "seas#create"
+
+  # #READ
+  # get 'seas', to: "seas#index"
+  # # , as: 'seas_path'
+  # get 'seas/:id', to: "seas#show"
+  # # , as: 'sea_path(:id)'
+
+  # #UPDATE
+  # get 'seas/:id', to: "seas#edit"
+  # # , as: 'edit_sea_path(:id)'
+  # patch 'seas/:id', to: "seas#update"
+
+  # #DELETE
+  # delete 'seas/:id', to: "seas#delete"
+
 end
+
+
